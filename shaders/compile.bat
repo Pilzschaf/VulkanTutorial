9c0078f0268bde3074eb7f374450007f89bf2527
Pilzschaf <pilzschaf@t-online.de>
2021-10-06 16:29:47 +0200

    Added shaders

---
+++ shaders/compile.bat
@@ -0,0 +1,2 @@
+glslc.exe -fshader-stage=vert triangle_vert.glsl -o triangle_vert.spv
+glslc.exe -fshader-stage=frag triangle_frag.glsl -o triangle_frag.spv
---
+++ shaders/triangle_frag.glsl
@@ -0,0 +1,7 @@
+#version 450 core
+
+layout(location = 0) out vec4 color_out;
+
+void main() {
+	color_out = vec4(1.0, 1.0, 0.0, 1.0);
+}
